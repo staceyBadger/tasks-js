b0868dc2fc20d429c5caf2993c421700db5d2855
staceyBadger <80848175+staceyBadger@users.noreply.github.com>
2021-08-29 21:20:34 +0500

    Задачи по JS

---
+++ object-selection.js
@@ -0,0 +1,30 @@
+const workers = [
+    {"name":"John","salary":500},
+    {"name":"Mike","salary":1300},
+    {"name":"Linda","salary":1500}
+];
+
+const getWorthyWorkers = workersArr => {
+    const worthyWorkers = [];
+
+    workersArr.forEach(currentWorker => {
+        if(currentWorker.salary > 1000) {
+            worthyWorkers.push(currentWorker.name)
+        }
+    })
+    return worthyWorkers;
+   
+
+    /*for (let i = 0; i < workersArr.length; i++) {
+        const currentWorker = workersArr[i];
+
+        if (currentWorker.salary > 1000) {
+            worthyWorkers.push(currentWorker.name);
+        }
+    }*/
+
+    
+}
+
+console.log(getWorthyWorkers(workers))
+  
---
+++ string-filters.js
@@ -0,0 +1,17 @@
+const string = "Привет! Как дела?";
+
+const vowels = ["а", "у", "о", "ы", "и", "э", "я", "ю", "ё", "е"];
+
+const getVowels = stringToFilter => {
+    let extractedVowels = "";
+    for (let i = 0; i < stringToFilter.length; i++) {
+        const currentLetter = stringToFilter[i].toLowerCase();
+
+        if(vowels.includes(currentLetter)) {
+            extractedVowels += currentLetter;
+        }
+    }
+
+    return extractedVowels;
+}
+console.log(getVowels(string));
